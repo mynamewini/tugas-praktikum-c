978b69c143410c9274c2da82e0a6919ac5fd3615
Wini Suryani <147029017+mynamewini@users.noreply.github.com>
2023-10-12 13:44:28 +0700

    Add files via upload

---
+++ PERTEMUAN 1/TugasMandiri1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int NIM=2143020;
+	char* Nama="Wini Suryani";
+	char* Jurusan="Teknik Informatika";
+	
+	printf("Biodata\n");
+	printf("NIM 	: %d \n", NIM);
+	printf("Nama 	: %s \n", Nama);
+	printf("Jurusan : %s", Jurusan);
+	return 0;
+}
---
+++ PERTEMUAN 1/TugasMandiri2.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main()
+{
+	int a,b,t;
+	a=10;
+	b=7;
+	t=a*b;
+	printf("Hasil Luas Persegi %i",t);
+	return 0;
+}
